6b214f178876743d7992859538692c26060f7934
lucyconway <lucymac@live.unc.edu>
2023-03-02 20:55:06 -0500

    rock paper spock attempt

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,19 +5,33 @@ import minimist from "minimist";
 const args = minimist(process.argv.slice(2));
 
 if(args.h || args.help){
-    console.log('Usage: node-rps [SHOT] \\nPlay Rock Paper Scissors (RPS) \\n \\n  -h, --help      display this help message and exit \\n  -r, --rules     display the rules and exit \\n\\nExamples: \\n  node-rps        Return JSON with single player RPS result. \\n                  e.g. {"player":"rock"} \\n  node-rps rock   Return JSON with results for RPS played against a simulated opponent. \\n                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log('Usage: node-rps [SHOT] \\nPlay Rock Paper Scissors (RPS) ' +
+        '\\n \\n  -h, --help      display this help message and exit \\n  ' +
+        '-r, --rules     display the rules and exit \\n\\n' +
+        'Examples: \\n  node-rps        Return JSON with single player RPS result. ' +
+        '\\n                  e.g. {"player":"rock"} \\n  ' +
+        'node-rps rock   Return JSON with results for RPS played against a simulated opponent. ' +
+        '\\n                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
     process.exit(0);
 }
 
 if(args.r || args.rules){
-    console.log('Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper \n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors');
+    console.log('Rules for Rock Paper Scissors:\n\n  ' +
+        '- Scissors CUTS Paper \n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors');
     process.exit(0);
 }
 
 let play = rps(args._[0]);
 if(play == 'out-of-range-error'){
     console.error(args._[0] + ' is out of range.\n');
-    console.log('Usage: node-rps [SHOT] \nPlay Rock Paper Scissors (RPS) \n \n  -h, --help      display this help message and exit \n  -r, --rules     display the rules and exit \n\nExamples: \n  node-rps        Return JSON with single player RPS result. \n                  e.g. {"player":"rock"} \n  node-rps rock   Return JSON with results for RPS played against a simulated opponent. \n                  e.g {"player":"rock","opponent":"scissors","result":"win"}');
+    console.log('Usage: node-rps [SHOT] ' +
+        '\nPlay Rock Paper Scissors (RPS) \n \n  ' +
+        '-h, --help      display this help message and exit \n  ' +
+        '-r, --rules     display the rules and exit \n' +
+        '\nExamples: \n  node-rps        Return JSON with single player RPS result. ' +
+        '\n                  e.g. {"player":"rock"} \n  node-rps rock   ' +
+        'Return JSON with results for RPS played against a simulated opponent. \n                  ' +
+        'e.g {"player":"rock","opponent":"scissors","result":"win"}');
     console.log('\n');
     console.log('Rules for Rock Paper Scissors:\n\n  - Scissors CUTS Paper \n  - Paper COVERS Rock\n  - Rock CRUSHES Scissors');
 
